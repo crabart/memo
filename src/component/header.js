7479da7121b96e4ce27b09606b799da7a7827158
trabarc <trabarc@hotmail.com>
2022-09-10 17:45:08 +0900

    ヘッダーとフッター追加

---
+++ src/component/header.js
@@ -0,0 +1,30 @@
+function Header() {
+  return (
+    <header>
+      <div className="headerTitle">メモアプリケーションテスト</div>
+
+      <div className="linkHeader">
+        <a href="/logout">ログアウト</a>
+      </div>
+      <style jsx> {`
+        header {
+          height: 100px;
+          background-color: #ffeddb;
+        }
+        
+        .headerTitle {
+          float: left;
+          font-size: 24px;
+          margin: 30px 20px;
+        }
+        
+        .linkHeader {
+          float: right;
+          margin-top: 30px;
+        }
+      `}</style>
+    </header>
+  );
+}
+
+export default Header;
