fd954bf4d44c40e0fc415c0c79dc1eac8edbbe29
crabart <crabartmono@gmail.com>
2022-09-30 20:09:31 +0900

    ユーザー登録されるよう修正

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const app = express();
 
 app.use(express.static('build'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.post(
   '/signup',
   async (req, res, next) => {
     try {
       const email = req.body.email;
       const user = await userBase.get(email);
+
       if (user) {
         res.redirect('/signup?error=100');
       } else {
@@ -23,12 +27,13 @@ app.post(
     }
   },
   async (req, res) => {
+    const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
 
     try {
       var hash = bcrypt.hashSync(password, 10);
-      await userBase.put({ password: hash }, email);
+      await userBase.put({ name: name, password: hash }, email);
       res.redirect('/');
     } catch (error) {
       res.send(error);
