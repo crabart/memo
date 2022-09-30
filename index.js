b2d0309f134dc5a7e6df672124cbf7d50c20adb8
crabart <crabartmono@gmail.com>
2022-09-30 19:38:24 +0900

    ユーザー登録操作追加

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-// install express with `npm install express` 
+// install express with `npm install express`
 const express = require('express');
+const bcrypt = require('bcrypt');
+const { Base } = require('deta');
+const userBase = Base('users');
 const app = express();
 
 app.use(express.static('build'));
 
+app.post(
+  '/signup',
+  async (req, res, next) => {
+    try {
+      const email = req.body.email;
+      const user = await userBase.get(email);
+      if (user) {
+        res.redirect('/signup?error=100');
+      } else {
+        next();
+      }
+    } catch (error) {
+      res.send(error);
+    }
+  },
+  async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+      var hash = bcrypt.hashSync(password, 10);
+      await userBase.put({ password: hash }, email);
+      res.redirect('/');
+    } catch (error) {
+      res.send(error);
+    }
+  }
+);
+
 app.get('/', (req, res) => {
-  res.send('index.html')
+  res.send('index.html');
 });
 
 // export 'app'
