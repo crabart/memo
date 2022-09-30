515c002392f8fb3444e554fb26a57fba85dd1171
crabart <crabartmono@gmail.com>
2022-09-30 20:46:49 +0900

    expressでreactのルーティングができるよう修正

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const { Base } = require('deta');
 const userBase = Base('users');
 const app = express();
+const path = require('path');
 
 app.use(express.static('build'));
 
@@ -41,8 +42,12 @@ app.post(
   }
 );
 
-app.get('/', (req, res) => {
-  res.send('index.html');
+//ミドルウエアでstaticパスを追加（ただ、これだけだと直アクセスや無いpathだと動かない）
+app.use(express.static(path.join(__dirname, '.', 'build')));
+
+//これを追加（全てをindex.htmlにリダイレクト。いわゆるrewrite設定）
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, '.', 'build', 'index.html'));
 });
 
 // export 'app'
